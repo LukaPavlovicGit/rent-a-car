178f546c1339e73f51d956ab3be53ea95fd3df2a
Luka <lpavlovic11521rn@raf.rs>
2022-12-14 00:47:19 +0100

    initial commit

---
+++ notification-service/src/test/java/raf/rentacar/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package raf.rentacar.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

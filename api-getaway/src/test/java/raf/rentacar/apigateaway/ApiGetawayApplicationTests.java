f9d8b4cc221984731f73eacce644a41868be0fd0
Luka <lpavlovic11521rn@raf.rs>
2022-12-14 02:02:49 +0100

    added getaway and eureka, and configuration has been set

---
+++ api-getaway/src/test/java/raf/rentacar/apigateaway/ApiGetawayApplicationTests.java
@@ -0,0 +1,13 @@
+package raf.rentacar.apigateaway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGetawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ eureka/src/test/java/raf/rentacar/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package raf.rentacar.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

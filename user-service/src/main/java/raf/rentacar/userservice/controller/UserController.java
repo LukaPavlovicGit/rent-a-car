c60e968757da3259fc5192a44f29b7be7a2e03f6
Luka <lpavlovic11521rn@raf.rs>
2022-12-15 01:35:58 +0100

    updates

--- user-service/src/main/java/raf/rentacar/userservice/controller/UserController.java
+++ user-service/src/main/java/raf/rentacar/userservice/controller/UserController.java
@@ -1,16 +1,12 @@
 package raf.rentacar.userservice.controller;
 
-import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import raf.rentacar.userservice.dto.ClientDto;
-import raf.rentacar.userservice.dto.ManagerDto;
-import raf.rentacar.userservice.dto.UserDto;
+import raf.rentacar.userservice.dto.*;
 import raf.rentacar.userservice.secutiry.CheckSecurity;
 import raf.rentacar.userservice.secutiry.SecurityAspect;
 import raf.rentacar.userservice.service.UserService;
@@ -51,6 +47,11 @@ public class UserController {
         return new ResponseEntity<>(null, HttpStatus.CREATED);
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponseDto> login(@RequestBody TokenRequestDto tokenRequestDto) {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
     @PutMapping("/update-admin")
     @CheckSecurity(roles = {"ROLE_ADMIN"})
     public ResponseEntity<UserDto> updateAdmin(@RequestHeader("authorization") String authorization,
@@ -61,7 +62,7 @@ public class UserController {
 
     @PutMapping("/update-manager")
     @CheckSecurity(roles = {"ROLE_MANAGER"})
-    public ResponseEntity<ClientDto> updateManager(@RequestHeader("authorization") String authorization,
+    public ResponseEntity<ManagerDto> updateManager(@RequestHeader("authorization") String authorization,
                                                    @RequestBody ManagerDto managerDto){
         //Long clientId = securityAspect.getUserId(authorization);
         return new ResponseEntity<>(null, HttpStatus.OK);
@@ -81,4 +82,23 @@ public class UserController {
         return new ResponseEntity<>(null, HttpStatus.OK);
     }
 
+    @PostMapping("/account-activation")
+    @CheckSecurity(roles = {"ROLE_ADMIN", "ROLE_MANAGER, ROLE_USER"})
+    public ResponseEntity<Object> accountActivation(@RequestHeader("authorization") String authorization){
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @PostMapping("/ban-user/{id}")
+    @CheckSecurity(roles = {"ROLE_ADMIN"})
+    public ResponseEntity<Object> banUser(@RequestHeader("authorization") String authorization,
+                                          @PathVariable("id") Long id){
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @PostMapping("/remove-ban-user/{id}")
+    @CheckSecurity(roles = {"ROLE_ADMIN"})
+    public ResponseEntity<Object> removeBanUser(@RequestHeader("authorization") String authorization,
+                                                @PathVariable("id") Long id){
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
 }

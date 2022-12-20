bda49b2f944b21eeb3292d105959dd9676670f99
Luka <lpavlovic11521rn@raf.rs>
2022-12-20 14:15:59 +0100

    updated ReservationController and ReservationService

--- reservation-service/src/main/java/raf/rentacar/reservationservice/controller/ReservationController.java
+++ reservation-service/src/main/java/raf/rentacar/reservationservice/controller/ReservationController.java
@@ -1,9 +1,71 @@
 package raf.rentacar.reservationservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import raf.rentacar.reservationservice.dto.CompanyDto;
+import raf.rentacar.reservationservice.dto.ReservationDto;
+import raf.rentacar.reservationservice.dto.VehicleDto;
+import raf.rentacar.reservationservice.secutiry.CheckSecurity;
+import raf.rentacar.reservationservice.service.ReservationService;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequestMapping("/reservations")
 public class ReservationController {
+
+    private ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping
+    @CheckSecurity(roles = {"ROLE_ADMIN"})
+    public ResponseEntity<Page<ReservationDto>> getReservations(@RequestHeader("Authorization") String authorization,
+                                                             @ApiIgnore Pageable pageable) {
+        return new ResponseEntity<>(reservationService.getReservations(pageable), HttpStatus.OK);
+    }
+
+    @GetMapping()
+    @CheckSecurity(roles = {"ROLE_MANAGER"})
+    public ResponseEntity<Page<ReservationDto>> getReservationsByCompany(@RequestHeader("Authorization") String authorization) {
+        return new ResponseEntity<>(reservationService.getReservationsByCompany(authorization), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    @CheckSecurity(roles = {"ROLE_ADMIN", "ROLE_MANAGER"})
+    public ResponseEntity<ReservationDto> getReservation(@RequestHeader("Authorization") String authorization,
+                                                         @PathVariable Long id) {
+        return new ResponseEntity<>(reservationService.getReservation(authorization, id), HttpStatus.OK);
+    }
+    @PostMapping
+    @CheckSecurity(roles = {"ROLE_CLIENT"})
+    public ResponseEntity<ReservationDto> createReservation(@RequestHeader("Authorization") String authorization,
+                                                            @RequestBody ReservationDto reservationDto) {
+        return new ResponseEntity<>(reservationService.createReservation(authorization, reservationDto), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    @CheckSecurity(roles = {"ROLE_CLIENT"})
+    public ResponseEntity<ReservationDto> cancelReservation(@RequestHeader("Authorization") String authorization,
+                                                            @PathVariable Long id) {
+        return new ResponseEntity<>(reservationService.cancelReservation(authorization, id), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    @CheckSecurity(roles = {"ROLE_CLIENT"})
+    public ResponseEntity<ReservationDto> updateReservation(@RequestHeader("Authorization") String authorization,
+                                                            @PathVariable Long id) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    @CheckSecurity(roles = {"ROLE_MANAGER"})
+    public ResponseEntity<ReservationDto> deleteReservation(@RequestHeader("Authorization") String authorization,
+                                                        @PathVariable Long id) {
+        return new ResponseEntity<>(reservationService.deleteReservation(authorization, id), HttpStatus.OK);
+    }
 }
--- reservation-service/src/main/java/raf/rentacar/reservationservice/service/ReservationService.java
+++ reservation-service/src/main/java/raf/rentacar/reservationservice/service/ReservationService.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
@@ -13,11 +14,14 @@ import raf.rentacar.reservationservice.domain.Company;
 import raf.rentacar.reservationservice.domain.Reservation;
 import raf.rentacar.reservationservice.domain.Vehicle;
 import raf.rentacar.reservationservice.dto.DiscountDto;
+import raf.rentacar.reservationservice.dto.MessageTransferDto;
 import raf.rentacar.reservationservice.dto.ReservationDto;
 import raf.rentacar.reservationservice.exception.InvalidArguments;
 import raf.rentacar.reservationservice.exception.NotFoundException;
+import raf.rentacar.reservationservice.exception.OperationNotAllowed;
 import raf.rentacar.reservationservice.exception.UnauthorizedOperation;
 import raf.rentacar.reservationservice.mapper.Mapper;
+import raf.rentacar.reservationservice.messageHelper.MessageHelper;
 import raf.rentacar.reservationservice.repository.CompanyRepository;
 import raf.rentacar.reservationservice.repository.ReservationRepository;
 import raf.rentacar.reservationservice.repository.VehicleRepository;
@@ -25,9 +29,13 @@ import raf.rentacar.reservationservice.secutiry.tokenService.TokenService;
 import io.github.resilience4j.retry.Retry;
 
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
@@ -42,16 +50,20 @@ public class ReservationService {
     private Mapper mapper;
     private RestTemplate userServiceRestTemplate;
     private Retry userServiceRetry;
+    private JmsTemplate jmsTemplate;
+    private MessageHelper messageHelper;
 
-    public ReservationService(ReservationRepository reservationRepository, VehicleRepository vehicleRepository, CompanyRepository companyRepository,
-                              TokenService tokenService, RestTemplate userServiceRestTemplate, Mapper mapper, Retry userServiceRetry) {
+    public ReservationService(ReservationRepository reservationRepository, VehicleRepository vehicleRepository, CompanyRepository companyRepository, TokenService tokenService,
+                              Mapper mapper, RestTemplate userServiceRestTemplate, Retry userServiceRetry, JmsTemplate jmsTemplate, MessageHelper messageHelper) {
         this.reservationRepository = reservationRepository;
         this.vehicleRepository = vehicleRepository;
         this.companyRepository = companyRepository;
         this.tokenService = tokenService;
-        this.userServiceRestTemplate = userServiceRestTemplate;
         this.mapper = mapper;
+        this.userServiceRestTemplate = userServiceRestTemplate;
         this.userServiceRetry = userServiceRetry;
+        this.jmsTemplate = jmsTemplate;
+        this.messageHelper = messageHelper;
     }
 
     //admin
@@ -59,7 +71,7 @@ public class ReservationService {
         return reservationRepository.findAll(pageable).map(mapper::reservationToReservationDto);
     }
     //manager
-    public Page<ReservationDto> getReservationsByCompany(String authorization, Pageable pageable){
+    public Page<ReservationDto> getReservationsByCompany(String authorization){
         Claims claims = tokenService.parseToken(authorization.split(" ")[1]);
         Long managerId = claims.get("id", Integer.class).longValue();
         Company company = companyRepository.findCompanyByManagerId(managerId)
@@ -89,20 +101,14 @@ public class ReservationService {
         Claims claims = tokenService.parseToken(authorization.split(" ")[1]);
         Long clientId = claims.get("id", Integer.class).longValue();
         String email = claims.get("email", String.class);
+        String firstname = claims.get("firstname", String.class);
+        String lastname = claims.get("lastname", String.class);
+
         Vehicle vehicle = vehicleRepository.findById(reservationDto.getVehicleId())
                 .orElseThrow(() -> new NotFoundException(String.format("The vehicle with id: %d is not found!", reservationDto.getVehicleId())));
 
-      ResponseEntity<DiscountDto> discountDtoResponseEntity =
-              Retry.decorateSupplier(
-                      userServiceRetry,
-                      () -> userServiceRestTemplate.exchange(
-                              "/users/discount/"+clientId,
-                              HttpMethod.GET,
-                              null,
-                              DiscountDto.class)
-              ).get();
-
-        System.out.println(discountDtoResponseEntity.getBody().getDiscount());
+        if(!vehicle.isAvailable())
+            throw new OperationNotAllowed(String.format("The car with id: %d is not available!", vehicle.getId()));
 
         Date now = Date.valueOf(LocalDate.now());
         Date start = reservationDto.getStart();
@@ -113,26 +119,104 @@ public class ReservationService {
 
         long startInMs = start.getTime();
         long endInMs = end.getTime();
-
         long timeDiff = Math.abs(endInMs - startInMs);
         long daysDiff = TimeUnit.DAYS.convert(timeDiff, TimeUnit.MILLISECONDS);
 
+        ResponseEntity<DiscountDto>
+              discountDtoResponseEntity = Retry.decorateSupplier(
+                      userServiceRetry,
+                      () -> userServiceRestTemplate.exchange(
+                              "/users/discount/"+clientId,
+                              HttpMethod.GET,
+                              null,
+                              DiscountDto.class)
+              ).get();
+
+        DiscountDto discountDto = discountDtoResponseEntity.getBody();
+        System.out.println("RESERVATION CREATION - discount value: "+discountDto.getDiscount());
+
         vehicle.setAvailable(false);
         vehicleRepository.save(vehicle);
-        Long companyId = vehicle.getCompany().getId();
+
+        BigDecimal percentage = new BigDecimal(discountDto.getDiscount()).setScale(2, RoundingMode.HALF_DOWN);
+        BigDecimal hundred = new BigDecimal(100).setScale(2,RoundingMode.HALF_DOWN);
+        Double discount = percentage.divide(hundred).doubleValue();
+        Double oneDayPrice = vehicle.getPrice() - discount;
+        Double totalPrice = daysDiff * oneDayPrice;
+
         Reservation reservation = mapper.reservationDtoToReservation(reservationDto);
         reservation.setUserId(clientId);
         reservation.setUserMail(email);
-        reservation.setCompanyId(companyId);
+        reservation.setCompanyId(vehicle.getCompany().getId());
+        reservation.setTotalPrice(totalPrice);
         reservationRepository.save(reservation);
+
+        userServiceRestTemplate.exchange(
+                "/users/increment-days/"+clientId,
+                HttpMethod.POST,
+                null,
+                ResponseEntity.class,
+                (int) daysDiff
+        );
+        MessageTransferDto messageTransferDto = new MessageTransferDto(
+            firstname,
+            lastname,
+            vehicle.getName(),
+            email,
+            true
+        );
+        jmsTemplate.convertAndSend("reservation_queue", messageHelper.createTextMessage(messageTransferDto));
+
         return mapper.reservationToReservationDto(reservation);
     }
 
-    //manager client
-    public ReservationDto cancelReservation(Long id){
+    //client
+    public ReservationDto cancelReservation(String authorization, Long id){
+        Claims claims = tokenService.parseToken(authorization.split(" ")[1]);
+        Long clientId = claims.get("id", Integer.class).longValue();
+        String email = claims.get("email", String.class);
+        String firstname = claims.get("firstname", String.class);
+        String lastname = claims.get("lastname", String.class);
+
         Reservation reservation = reservationRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException(String.format("The reservation with id: %d is not found!", id)));
+
+        Date now = Date.valueOf(LocalDate.now());
+        Date start = reservation.getStart();
+        Date end = reservation.getEnd();
+
+        if(start.before(now))
+            throw new OperationNotAllowed("The reservation is not possible to cancel!");
+
+        Vehicle vehicle = vehicleRepository.findById(reservation.getVehicleId())
+                .orElseThrow(() -> new NotFoundException(String.format("The vehicle with id: %d is not found!", reservation.getVehicleId())));
+
         reservation.setCanceled(true);
+        vehicle.setAvailable(true);
+        reservationRepository.save(reservation);
+        vehicleRepository.save(vehicle);
+
+        long startInMs = start.getTime();
+        long endInMs = end.getTime();
+        long timeDiff = Math.abs(endInMs - startInMs);
+        long daysDiff = TimeUnit.DAYS.convert(timeDiff, TimeUnit.MILLISECONDS);
+
+        userServiceRestTemplate.exchange(
+                "/users/decrement-days/"+clientId,
+                HttpMethod.POST,
+                null,
+                ResponseEntity.class,
+                (int) daysDiff
+        );
+        MessageTransferDto messageTransferDto = new MessageTransferDto(
+                firstname,
+                lastname,
+                vehicle.getName(),
+                email,
+                1
+        );
+        jmsTemplate.convertAndSend("reservation_cancellation_queue", messageHelper.createTextMessage(messageTransferDto));
+
         return mapper.reservationToReservationDto(reservation);
     }
 

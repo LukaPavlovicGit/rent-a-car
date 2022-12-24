8b769b3a93e7e650866d36cb635924a2d1d665b4
Luka <lpavlovic11521rn@raf.rs>
2022-12-24 22:41:25 +0100

    small changes

--- notification-service/src/main/java/raf/rentacar/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/raf/rentacar/notificationservice/NotificationServiceApplication.java
@@ -1,7 +1,11 @@
 package raf.rentacar.notificationservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class NotificationServiceApplication {
@@ -9,5 +13,16 @@ public class NotificationServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
+
+//	@Component
+//	public class ClearCacheTask {
+//		@Autowired
+//		private CacheManager cacheManager;
+//
+//		@Scheduled(fixedRateString = "${clear.all.cache.fixed.rate}", initialDelayString = "${clear.all.cache.init.delay}") // reset cache every hr, with delay of 1hr after app start
+//		public void reportCurrentTime() {
+//			cacheManager.getCacheNames().parallelStream().forEach(name -> cacheManager.getCache(name).clear());
+//		}
+//	}
 
 }
--- notification-service/src/main/java/raf/rentacar/notificationservice/service/EmailPatternsService.java
+++ notification-service/src/main/java/raf/rentacar/notificationservice/service/EmailPatternsService.java
@@ -5,5 +5,5 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-public class EmailService {
+public class EmailPatternsService {
 }

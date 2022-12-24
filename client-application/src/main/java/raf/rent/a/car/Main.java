ffb3013feaabc4920bd31f6ac6aad8b4c085a590
Luka Pavlovic <92918995+LukaPavlovicGit@users.noreply.github.com>
2022-12-24 20:40:31 +0100

    Delete client-application directory

--- client-application/src/main/java/raf/rent/a/car/Main.java
+++
@@ -1,8 +0,0 @@
-package raf.rent.a.car;
-
-public class Main {
-    public static void main(String[] args) {
-        MainFrame mainFrame = MainFrame.getInstance();
-        mainFrame.setVisible(true);
-    }
-}
